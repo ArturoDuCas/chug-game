7687f20416bccfc5d7a57b90c4062ce12e1d3771
ArturoDuCas <a00833516@tec.mx>
2024-03-28 15:06:34 -0600

    feat: add view transitions

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import Button from "@/components/button";
+import LinkTransition from "@/utils/LinkTransition";
+
 export default async function Index() {
 
   return (
-    <div>
-      <h1 className="text-8xl font-bold tracking-wide border rounded p-4">Chug.</h1>
+    <div className="flex flex-col gap-4">
+      <div className="w-full bg-slate-50 rounded p-4 shadow">
+        <h1 className="text-8xl font-bold tracking-wide bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">Chug.</h1>
+      </div>
+      <div className="w-full flex justify-around items-center gap-4">
+        <LinkTransition href={"/join"} className="flex-1">
+          <Button text="Join"  maxWidth />
+        </LinkTransition>
+        <LinkTransition href={"/create"} className="flex-1">
+          <Button text="Create" variant="outlined" maxWidth />
+        </LinkTransition>
+      </div>
     </div>
   );
 }
---
+++ components/button.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps {
+  text: string;
+  variant?: "default" | "outlined";
+  maxWidth?: boolean;
+}
+
+const Button = ({ text, variant = "default", maxWidth = false} : ButtonProps) => {
+  // Classes applied for all buttons
+  let className = "p-2 rounded transition-colors " + " ";
+
+  // Set the button to full width if maxWidth is true
+  if (maxWidth) {
+    className += "w-full ";
+  }
+
+  // Set the button variant
+  switch (variant) {
+    case "default":
+      className += "bg-slate-50 hover:bg-slate-50/80 text-slate-950";
+      break;
+    case "outlined":
+      className += "border border-slate-50 hover:bg-slate-50 text-slate-50 hover:text-slate-950";
+      break;
+  }
+
+  return <button className={className}>{text}</button>;
+
+};
+
+export default Button;

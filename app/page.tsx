67a17ba36a64539b763bdfd34c12bfa3b072c323
ArturoDuCas <a00833516@tec.mx>
2024-03-27 22:37:45 -0600

    feat: add ui components to welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import AddPlayer from "@/components/add-player";
 
 export default function Home() {
   return (
-      <main>
-        <h1>Chug 🍺</h1>
-        <Button className="">Start</Button>
+      <main className="flex flex-col gap-4 justify-center items-center h-screen">
+        <h1 className="text-8xl font-bold text-slate-100">
+          Chug.
+        </h1>
+        <Button className="dark" size="lg">
+              Start
+        </Button>
+        <AddPlayer/>
       </main>
   );
 }

623b4e7efaf82c67d794c789ec624b45c9a653ae
ArturoDuCas <a00833516@tec.mx>
2024-03-28 14:24:26 -0600

    fix: height in ios safari

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default async function Index() {
 
   return (
     <div>
-      <h1 className="text-8xl font-bold tracking-wide animate-bounce border p-4">Chug.</h1>
+      <h1 className="text-8xl font-bold tracking-wide border rounded p-4">Chug.</h1>
     </div>
   );
 }

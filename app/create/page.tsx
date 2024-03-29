826a17761c437ed27c1c61ce486b7d170408c22e
ArturoDuCas <a00833516@tec.mx>
2024-03-29 09:52:15 -0600

    fix: add skeleton to room id suspense

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,12 +3,13 @@ import RoundsInput from "@/components/rounds-input";
 import SpecialButton from "@/components/special-button";
 import GoBack from "@/components/go-back";
 import {Suspense} from "react";
+import {Skeleton} from "@/components/ui/skeleton";
 
 const Page = () => {
   return (
       <div className="w-full p-6 flex flex-col gap-4">
         <GoBack />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Skeleton className="rounded w-full h-[58px]"></Skeleton>}>
             <RoomId />
           </Suspense>
         <RoundsInput/>

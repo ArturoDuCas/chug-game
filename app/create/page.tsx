a4671e160b149490d3c8ff027ae3ee188a38e22d
ArturoDuCas <a00833516@tec.mx>
2024-03-28 23:42:28 -0600

    fix: wrap component with suspense

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,12 +2,15 @@ import RoomId from "@/components/room-id";
 import RoundsInput from "@/components/rounds-input";
 import SpecialButton from "@/components/special-button";
 import GoBack from "@/components/go-back";
+import {Suspense} from "react";
 
 const Page = () => {
   return (
       <div className="w-full p-6 flex flex-col gap-4">
         <GoBack />
-        <RoomId />
+          <Suspense fallback={<div>Loading...</div>}>
+            <RoomId />
+          </Suspense>
         <RoundsInput/>
         <SpecialButton text="Start Now 😈"/>
 
